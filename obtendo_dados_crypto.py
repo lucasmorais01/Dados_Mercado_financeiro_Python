6a66afc45fc75584bf558a53c19ab766379f55cf
lugabsa <lucasgabrielsm7@gmail.com>
2024-05-03 12:20:58 -0300

    Estudos_analises_dados_python

--- obtendo_dados_crypto.py
+++
@@ -1,7 +0,0 @@
-from cryptocmd import CmcScraper
-
-scraper = CmcScraper("ETH")
-
-dados_crypto = scraper.get_dataframe()
-
-print(dados_crypto)
--- obtendo_dados_yfinance.py
+++
@@ -1,8 +0,0 @@
-import  yfinance as yf  
-
-lista_empresas = ["PETR4.SA", "ITAU",]
-
-dados_empresa = yf.download(lista_empresas)
-
-print(dados_empresa)
-
